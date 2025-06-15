596f4899a8c9994ef00798aaf8d86c8f9016bc8d
Linda061 <linda061f@gmail.com>
2025-06-15 18:35:39 +0300

    challenge5

---
+++ DataStructures/LinkedList/app.js
@@ -0,0 +1,21 @@
+const LinkedList = require("./linkedList");
+
+const list = new LinkedList();
+
+list.add(5);
+list.add(10);
+list.add(20);
+list.add(30);
+
+list.print();
+
+
+list.remove(10);  
+list.print();  
+
+
+console.log(list.includes(20));  
+console.log(list.includes(10)); 
+
+list.insertAt(15, 2); 
+list.print();  
